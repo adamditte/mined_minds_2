433b533d25f33a0505e6814a90ae4f0d257e12b3
Adam Dittebrand <Adam Dittebrand>
2016-08-27 22:05:55 -0400

    made the test work for divisibles of 3, 5, and (3, 5)

--- test_mined_minds_2.rb
+++ test_mined_minds_2.rb
@@ -5,4 +5,30 @@ class TestMinedMindsFunction < Minitest::Test
 	def test_1_returns_1
 		assert_equal(1, mined_minds(1))
 	end
+
+	def test_3_returns_mined
+		assert_equal("mined", mined_minds(3))
+	end
+
+	def test_5_returns_minds
+		assert_equal("minds", mined_minds(5))
+	end
+
+	def test_9_returns_mined
+		assert_equal("mined", mined_minds(9))
+	end
+
+	def test_10_returns_minds
+		assert_equal("minds", mined_minds(10))
+	end
+
+	def test_15_returns_minedminds
+		assert_equal("minedminds", mined_minds(15))
+	end
+
+	def test_30_returns_minedminds
+		assert_equal("minedminds", mined_minds(30))
+	end
+
 end
+
