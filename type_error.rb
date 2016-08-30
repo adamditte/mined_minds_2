7fba6a3c6c4eedc2f1a1ccb77d83e30a13829398
Adam Dittebrand <Adam Dittebrand>
2016-08-30 14:05:41 -0400

    changed output

--- type_error.rb
+++ type_error.rb
@@ -2,5 +2,4 @@ def error_function(parameter1, parameter2)
 	parameter1 + parameter2
 end
 
-puts error_function("Amanda", "Laucher")
-puts error_function(2, "3".to_i)
+puts error_function(2.to_s,"Amanda")
