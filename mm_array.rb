4b8abfa470cc140942678caf7857eeec9bf8bf0d
Adam Dittebrand <Adam Dittebrand>
2016-09-01 19:49:39 -0400

    getting closer

--- mm_array.rb
+++ mm_array.rb
@@ -1,6 +1,22 @@
-mined_minds = [1, 2, 3, 4, 5]
+# def value
+# 	mined_minds = [*(1..100)]
+# 	mined_minds.each_index do |mined_minds|
+# 		puts value
+# 	end
+# end
+# value
 
-mined_minds.each do |mined_minds|
-	puts mined_minds
+def value
+  mined_minds = [*(1..100)]
+  mined_minds.each_index do |mined_minds|
+  	if mined_minds % 3 == 0 && mined_minds % 5 == 0
+		"minedminds"
+	elsif mined_minds % 5 == 0
+		"minds"
+	elsif mined_minds %  3 == 0
+		"mined"
+	else mined_minds
+	end
+  end
 end
-
+p value
