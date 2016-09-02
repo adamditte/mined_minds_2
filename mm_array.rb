81b52731c1131dd050c8f388dad834061ef88cd5
Adam Dittebrand <Adam Dittebrand>
2016-09-01 22:18:36 -0400

    changed code completely. Figuring out my errors. Getting closer still.

--- mm_array.rb
+++ mm_array.rb
@@ -21,16 +21,40 @@
 # end
 # p value
 
-def value
-  mined_minds = [*(1..100)]
-  mined_minds.each_index do |work|
-    val = mined_minds[work]
-    if val % 3 == 0 && val % 5 == 0
-      mined_minds[work] = 'MinedMinds'
-    elsif val % 3 == 0 or val == 3
-      mined_minds[work] = 'Mined'
-    elsif val % 5 == 0
-      mined_minds[work] = 'Minds'
+# def value
+#   mined_minds = [*(1..100)]
+#   mined_minds.each_index do |work|
+#     val = mined_minds[work]
+#     if val % 3 == 0 && val % 5 == 0
+#       mined_minds[work] = 'MinedMinds'
+#     elsif val % 3 == 0 or val == 3
+#       mined_minds[work] = 'Mined'
+#     elsif val % 5 == 0
+#       mined_minds[work] = 'Minds'
+#     end
+#   end
+# end
+
+# def mined_minds_array(*results)
+#     number = 0
+#     results.each_index do |result|
+#         results = number + result
+#     end
+#     results
+# end
+# p mined_minds_array
+
+def results
+    results = [*(1..100)]
+end
+def mined_minds_array()
+    number = 1
+    results.each_index do |result|
+        number = number + result
     end
-  end
+    if number = 3
+    	then "mined"
+    end
+    
+    results
 end
--- test_mm_array.rb
+++ test_mm_array.rb
@@ -5,19 +5,35 @@
 
 # 	def test_first_element_1
 # 		result = mined_minds(1)
-# 		assert_equal(1, result.first)
+# 		assert_equal(1, result.[0])
 # 	end
+# end
+# require "minitest/autorun"
+# require_relative "mm_array.rb"
+# class Test_mm_array < Minitest::Test 
+#     # def test_1_equals_1
+#     #   assert_equal(1, 1)
+#     # end
+#     def test_1_returns_1
+#         assert_equal(1, 1)
+#     end
+#     def test_3_returns_Mined
+#         assert_equal("Mined", 3)
+#     end
 # end
 require "minitest/autorun"
 require_relative "mm_array.rb"
-class Test_mm_array < Minitest::Test 
-    # def test_1_equals_1
-    #   assert_equal(1, 1)
-    # end
-    def test_1_returns_1
-        assert_equal(1, 1)
+class TestMMArray < Minitest::Test 
+    def test_first_element_is_1
+        result = mined_minds_array()
+        assert_equal(1, result.first)
     end
-    def test_3_returns_Mined
-        assert_equal("Mined", 3)
+    def test_second_element_is_2
+    	result = mined_minds_array()
+    	assert_equal(2, result[1])
+    end
+    def test_third_element_is_mined
+    	result = mined_minds_array()
+    	assert_equal("mined", result[2])
     end
 end
