c956f1389439fa005fa8718f0b37bbe5bb3f857b
Adam Dittebrand <Adam Dittebrand>
2016-09-06 14:29:34 -0400

    lottery test with working function.

---
+++ winningnumbers.rb
@@ -0,0 +1,5 @@
+def lottery(winners, my_num)
+	winners.include?(my_num)
+end
+
+		
---
+++ yournumbers.rb
@@ -0,0 +1,18 @@
+require "minitest/autorun"
+require_relative "winningnumbers.rb"
+
+class TestWinningNumbers < Minitest::Test 
+
+	def test_to_check_for_winner
+		winners = ["1234", "2567", "1984", "3736"]
+		my_num = "2567"
+		assert_equal(true, lottery(winners, my_num))
+	end
+	def test_for_non_winner
+		winners = ["1234", "2567", "1984", "3736"]
+		my_num = "2255"
+		assert_equal(false, lottery(winners, my_num))
+	end
+end
+
+
